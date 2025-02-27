17f641bd834724c3c24fa50097669d7f0c6e6ee2
Ury479 <140926655+Ury479@users.noreply.github.com>
2025-02-27 13:21:21 +0700

    new

--- src/main/java/camt/se234/lab10/Lab10Application.java
+++ src/main/java/camt/se234/lab10/Lab10Application.java
@@ -2,16 +2,13 @@ package camt.se234.lab10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// I have no ideas
 @SpringBootApplication
 public class Lab10Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Lab10Application.class, args);
     }
-
 
 }
